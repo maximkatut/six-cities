59f1996cd6713eaad93b313e5e79c9d31be4467c
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-17 19:01:01 -0400

    update tests for main and app components

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import App from './app.jsx';
 import renderer from 'react-test-renderer';
-import {rentNames} from '../../utils/testdata.js';
+import {offerCards} from '../../utils/test-data.js';
 
 describe(`App`, () => {
   it(`App should render correctly`, () => {
     const tree = renderer.create(
         <App
-          rentCount = {5}
-          rentNames = {rentNames}
+          offerCards = {offerCards}
         />)
       .toJSON();
 
