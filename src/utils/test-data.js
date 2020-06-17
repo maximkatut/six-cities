aed7683c361f082515daa568ca661c5e9aaa0454
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-17 18:44:42 -0400

    rename file by criteria

---
+++ src/utils/test-data.js
@@ -0,0 +1,20 @@
+export const offerCards = [
+  {
+    id: `1`,
+    imageSrc: `img/apartment-02.jpg`,
+    offerType: `Room`,
+    premium: false,
+    price: 200,
+    rate: 5,
+    title: `Nice, cozy, warm big bed apartment`
+  },
+  {
+    id: `2`,
+    imageSrc: `img/apartment-03.jpg`,
+    offerType: `House`,
+    premium: true,
+    price: 300,
+    rate: 4,
+    title: `Nice, cozy, warm big bed apartment`
+  }
+];
