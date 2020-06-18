13724460229dc31191a6f77ea3ff40d6bd68d35a
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-18 13:30:44 -0400

    use React Router for developing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 import Main from '../main/main.jsx';
+import Offer from '../offer/offer.jsx';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const handleMainCardTitleClick = () => {};
 
 const App = (props) => {
   const {offerCards} = props;
-  return <Main
-    offerCards = {offerCards}
-    onMainCardTitleClick = {handleMainCardTitleClick}
-  />;
+  const renderApp = () => {
+    return (
+      <Main
+        offerCards = {offerCards}
+        onMainCardTitleClick = {handleMainCardTitleClick}
+      />
+    );
+  };
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {renderApp()}
+        </Route>
+        <Route exact path="/dev-offer">
+          <Offer
+            offerCard={offerCards[0]}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
