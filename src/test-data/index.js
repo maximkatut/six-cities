e4a8a5f4fe31b402f4c80d344d468311f90d3887
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-18 18:46:44 -0400

    refactoring

--- src/test-data/index.js
+++ src/test-data/index.js
@@ -1,17 +1,46 @@
 export const offers = [
   {
+    appliences: [`Wi-Fi`, `Microwave`],
+    bedrooms: 3,
+    description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
+    guests: 2,
+    host: {
+      avatar: `img/avatar-angelina.jpg`,
+      name: `Max`,
+      super: true
+    },
     id: `1`,
-    mainImage: `img/apartment-02.jpg`,
-    offerType: `Room`,
-    premium: false,
+    imagesGallery: [
+      `img/apartment-01.jpg`,
+      `img/apartment-02.jpg`,
+      `img/apartment-03.jpg`,
+      `img/room.jpg`
+    ],
+    mainImage: `img/apartment-01.jpg`,
+    offerType: `House`,
+    premium: true,
     price: 200,
     rate: 5,
-    title: `Nice, cozy, warm big bed apartment`
+    title: `Nice, cozy, warm big bed house`
   },
   {
+    appliences: [`Wi-Fi`, `Microwave`],
+    bedrooms: 4,
+    description: [`An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
+    guests: 3,
+    host: {
+      avatar: `img/avatar-max.jpg`,
+      name: `Anton`,
+      super: false
+    },
     id: `2`,
-    mainImage: `img/apartment-03.jpg`,
-    offerType: `House`,
+    imagesGallery: [
+      `img/apartment-01.jpg`,
+      `img/apartment-03.jpg`,
+      `img/room.jpg`
+    ],
+    mainImage: `img/apartment-02.jpg`,
+    offerType: `Apartment`,
     premium: true,
     price: 300,
     rate: 4,
@@ -19,27 +48,3 @@ export const offers = [
   }
 ];
 
-export const offer = {
-  appliences: `Wi-Fi`,
-  bedrooms: 3,
-  description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
-  guests: 2,
-  host: {
-    avatar: `img/avatar-angelina.jpg`,
-    name: `Max`,
-    super: true
-  },
-  id: `1`,
-  imagesGallery: [
-    `img/apartment-01.jpg`,
-    `img/apartment-02.jpg`,
-    `img/apartment-03.jpg`,
-    `img/room.jpg`
-  ],
-  mainImage: `img/apartment-01.jpg`,
-  offerType: `House`,
-  premium: true,
-  price: 200,
-  rate: 5,
-  title: `Nice, cozy, warm big bed apartment`
-};
