fee091df4ba07992a0db2b25b85fd7a55d9d69d5
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-23 16:55:09 -0400

    create new component map that render a map with leaflet package

---
+++ src/components/map/map.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import leaflet from 'leaflet';
+import {offerPropType} from '../../types';
+import {MAP_ZOOM, MAP_ICON_SIZE, MAP_ICON_URL} from '../../const.js';
+
+class Map extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCity: 0
+    };
+    this._divRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const {offers, cities} = this.props;
+
+    const mapContainer = this._divRef.current;
+
+    const cityCoords = cities[this.state.activeCity].city;
+    const zoom = MAP_ZOOM;
+    const icon = leaflet.icon({
+      iconUrl: MAP_ICON_URL,
+      iconSize: MAP_ICON_SIZE
+    });
+
+    const map = leaflet.map(mapContainer, {
+      center: cityCoords,
+      zoom,
+      zoomControl: false,
+      marker: true,
+      layers: [
+        leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+          attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+        })]
+    });
+    offers.forEach((offer) => {
+      leaflet.marker(offer.coordinates, {icon}).addTo(map);
+    });
+  }
+
+  componentWillUnmount() {
+    const mapContainer = this._divRef.current;
+    mapContainer.remove();
+  }
+
+  render() {
+    return (
+      <section className="cities__map map"
+        ref={this._divRef}
+      >
+      </section>
+    );
+  }
+}
+
+Map.propTypes = {
+  offers: PropTypes.arrayOf(offerPropType).isRequired,
+  cities: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    city: PropTypes.arrayOf(PropTypes.number).isRequired
+  })).isRequired
+};
+
+export default Map;
