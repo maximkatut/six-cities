bc951c39ed6d63f6e918864da63428eaf11b1e0c
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-06-30 20:50:36 -0400

    city as part of offer object

--- src/const.js
+++ src/const.js
@@ -7,3 +7,6 @@ export const MAP_ZOOM = 12;
 export const MAP_ICON_SIZE = [27, 39];
 export const MAP_ICON_URL = `img/pin.svg`;
 export const MAP_ACTIVE_ICON_URL = `img/pin-active.svg`;
+
+// CITIES
+export const MAX_COUNT_CITIES = 6;
--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -31,11 +31,49 @@ const Appliences = [
   `Cabel TV`,
 ];
 
-const Coordinates = [
+const CoordinatesAmsterdam = [
   [52.3909553943508, 4.85309666406198],
   [52.369553943508, 4.85309666406198],
   [52.3909553943508, 4.929309666406198],
-  [52.3809553943508, 4.939309666406198]
+  [52.3809553943508, 4.939309666406198],
+  [52.3509553943508, 4.870309666406198],
+  [52.3709553943508, 4.939309666406198]
+];
+
+const CoordinatesParis = [
+  [48.8466, 2.3722],
+  [48.8416, 2.3392],
+  [48.8596, 2.3522],
+  [48.8366, 2.3592],
+  [48.8666, 2.3422],
+  [48.8266, 2.3622]
+];
+
+const Cities = [
+  {
+    name: `Amsterdam`,
+    coords: [52.38333, 4.9]
+  },
+  {
+    name: `Paris`,
+    coords: [48.8566, 2.3522]
+  },
+  {
+    name: `Cologne`,
+    coords: [50.9375, 6.9603]
+  },
+  {
+    name: `Brussels`,
+    coords: [50.8503, 4.3517]
+  },
+  {
+    name: `Hamburg`,
+    coords: [53.5511, 9.9937]
+  },
+  {
+    name: `Dusseldorf`,
+    coords: [51.2277, 6.7735]
+  }
 ];
 
 const Host = {
@@ -43,11 +81,12 @@ const Host = {
   AVATARS: [`img/avatar-angelina.jpg`, `img/avatar-max.jpg`],
 };
 
-const getRandomOffer = (coordinate) => {
+const getRandomOffer = (coordinate, city) => {
   return {
     appliences: getRandomArrayItems(Appliences),
     bedrooms: getRandomNumber(1, 3),
     coordinates: coordinate,
+    city,
     description: [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.`, `An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.`],
     guests: getRandomNumber(1, 8),
     host: {
@@ -88,6 +127,15 @@ const getRandomOffer = (coordinate) => {
   };
 };
 
-export const offers = Coordinates.map((coordinate) => {
-  return getRandomOffer(coordinate);
+export const offers = new Array(6).fill(``).map(() => {
+  const city = Cities[getRandomNumber(0, 1)];
+  let coordinate = [];
+  const coordA = CoordinatesAmsterdam;
+  const coordP = CoordinatesParis;
+  if (city.name === `Amsterdam`) {
+    coordinate = coordA.shift();
+  } else {
+    coordinate = coordP.shift();
+  }
+  return getRandomOffer(coordinate, city);
 });
