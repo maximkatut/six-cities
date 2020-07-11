e23254927dd7c46fc904f50e3db55f118bb75475
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-10 23:25:04 -0400

    add name-space for root reducer and add selectors for each reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,12 @@ import {combineReducers} from 'redux';
 import offers from './offers/offers-reducer';
 import map from './map/map-reducer';
 import user from './user/user-reducer';
+import data from './data/data-reducer';
+import NameSpace from './name-space';
 
 export default combineReducers({
-  offers,
-  map,
-  user
+  [NameSpace.OFFERS]: offers,
+  [NameSpace.MAP]: map,
+  [NameSpace.USER]: user,
+  [NameSpace.DATA]: data
 });
