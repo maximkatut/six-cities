9b48836599782d96c5f1adf2ce1615758d860ede
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-10 23:22:26 -0400

    setup redux-thunk and create api data request, create data and user actions and reducers

--- src/store.js
+++ src/store.js
@@ -1,7 +1,24 @@
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import {applyMiddleware} from 'redux';
+import {ActionCreator} from './actions/user-actions.js';
+import {createAPI} from './api';
+import AuthorizationStatus from './reducers/user/user-reducer';
+import {Operation} from './reducers/data/data-reducer';
+
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 export const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    compose(
+        applyMiddleware(thunk.withExtraArgument(api)),
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    )
 );
+
+store.dispatch(Operation.loadOffers());
