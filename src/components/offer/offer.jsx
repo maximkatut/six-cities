dc2c65fce76e6bbfdf16693b073fd819f5196954
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-15 12:18:27 -0400

    update offer component

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -9,9 +9,14 @@ import Header from '../header/header.jsx';
 import Map from '../map/map.jsx';
 import OffersList from '../offers-list/offers-list.jsx';
 import ReviewsList from '../reviews-list/reviews-list.jsx';
+import ReviewForm from '../review-form/review-form.jsx';
+import withReview from '../../hocs/with-review/with-review';
+
 import {getUserStatus} from '../../reducers/user/selectors';
 
-const Offer = ({offer, reviews, offersNearby}) => {
+const ReviewFormWrapped = withReview(ReviewForm);
+
+const Offer = ({offer, reviews, offersNearby, userStatus}) => {
   const {
     appliences,
     bedrooms,
@@ -23,8 +28,7 @@ const Offer = ({offer, reviews, offersNearby}) => {
     premium,
     price,
     rate,
-    title,
-    userStatus
+    title
   } = offer;
 
   return (
@@ -41,7 +45,6 @@ const Offer = ({offer, reviews, offersNearby}) => {
                   </div>
                 );
               })}
-
             </div>
           </div>
           <div className="property__container container">
@@ -117,53 +120,13 @@ const Offer = ({offer, reviews, offersNearby}) => {
               </div>
               <section className="property__reviews reviews">
                 <h2 className="reviews__title">Reviews · <span className="reviews__amount">{reviews.length}</span></h2>
+                <ReviewsList
+                  reviews={reviews}
+                />
                 {userStatus ?
-                  <ReviewsList
-                    reviews={reviews}
-                  /> : ``
+                  <ReviewFormWrapped/> : ``
                 }
-                <form className="reviews__form form" action="#" method="post">
-                  <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                  <div className="reviews__rating-form form__rating">
-                    <input className="form__rating-input visually-hidden" name="rating" defaultValue={5} id="5-stars" type="radio" />
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                    <input className="form__rating-input visually-hidden" name="rating" defaultValue={4} id="4-stars" type="radio" />
-                    <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                    <input className="form__rating-input visually-hidden" name="rating" defaultValue={3} id="3-stars" type="radio" />
-                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                    <input className="form__rating-input visually-hidden" name="rating" defaultValue={2} id="2-stars" type="radio" />
-                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                    <input className="form__rating-input visually-hidden" name="rating" defaultValue={1} id="1-star" type="radio" />
-                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                      <svg className="form__star-image" width={37} height={33}>
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-                  </div>
-                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={``} />
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                  To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                  </div>
-                </form>
+
               </section>
             </div>
           </div>
