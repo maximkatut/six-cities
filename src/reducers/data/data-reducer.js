3e9d1d4a817cf750190bad246cc58193b79c5450
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-16 17:47:37 -0400

    create new reducers and actions for adding offers to favorite list

--- src/reducers/data/data-reducer.js
+++ src/reducers/data/data-reducer.js
@@ -1,35 +1,40 @@
+import produce from 'immer';
 import {extend} from "../../utils";
 import {ActionCreator} from '../../actions/data-actions';
+import {ActionCreator as OffersActionCreator} from '../../actions/offers-actions';
 import {ActionType} from '../../actions/types';
-import getAdaptedOffers from '../../adapter/offers';
+import getAdaptedOffers, {adaptData as getAdaptedOffer} from '../../adapter/offers';
 import getAdaptedReviews from '../../adapter/reviews';
 import NameSpace from "../name-space";
 
 const initialState = {
-  offers: [],
+  favorites: [],
+  isBusy: false,
   isError: {
-    status: false,
-    message: ``
+    message: ``,
+    status: false
   },
-  isBusy: false
+  offers: []
 };
 
 export const Operation = {
   loadOffers: () => (dispatch, getState, api) => {
     return api.get(`/hotels`)
       .then((response) => {
-        const adapatedData = getAdaptedOffers(response.data);
-        dispatch(ActionCreator.loadOffers(adapatedData));
+        const adaptedData = getAdaptedOffers(response.data);
+        dispatch(ActionCreator.loadOffers(adaptedData));
       });
   },
+
   loadReviews: () => (dispatch, getState, api) => {
     const id = getState()[NameSpace.OFFERS].activeOffer.id;
     return api.get(`/comments/${id}`)
       .then((response) => {
-        const adapatedData = getAdaptedReviews(response.data);
-        dispatch(ActionCreator.loadReviews(adapatedData));
+        const adaptedData = getAdaptedReviews(response.data);
+        dispatch(ActionCreator.loadReviews(adaptedData));
       });
   },
+
   postReview: ({comment, rating}) => (dispatch, getState, api) => {
     const id = getState()[NameSpace.OFFERS].activeOffer.id;
     return api.post(`/comments/${id}`,
@@ -40,13 +45,14 @@ export const Operation = {
     )
       .then((response) => {
         dispatch(ActionCreator.setRequestStatusBusy(true));
-        const adapatedData = getAdaptedReviews(response.data);
-        dispatch(ActionCreator.loadReviews(adapatedData));
+        const adaptedData = getAdaptedReviews(response.data);
+        dispatch(ActionCreator.loadReviews(adaptedData));
       })
       .then(()=> {
         dispatch(ActionCreator.setRequestStatusBusy(false));
       });
   },
+
   loadOffersNearby: () => (dispatch, getState, api) => {
     const id = getState()[NameSpace.OFFERS].activeOffer.id;
     return api.get(`/hotels/${id}/nearby`)
@@ -55,6 +61,38 @@ export const Operation = {
         dispatch(ActionCreator.loadOffersNearby(adapatedData));
       });
   },
+
+  loadFavorites: () => (dispatch, getState, api) => {
+    return api.get(`/favorite`)
+      .then((response) => {
+        const adaptedData = getAdaptedOffers(response.data);
+        dispatch(ActionCreator.loadFavorites(adaptedData));
+      });
+  },
+
+  postFavorite: (id, isFavorite, isOfferPage = false) => (dispatch, getState, api) => {
+    const status = isFavorite ? 0 : 1;
+    return api.post(`/favorite/${id}/${status}`)
+      .then((response) => {
+        const adaptedOffer = getAdaptedOffer(response.data);
+        dispatch(ActionCreator.postFavorite(adaptedOffer));
+        dispatch(Operation.loadFavorites());
+        if (isOfferPage) {
+          dispatch(Operation.loadOffersNearby());
+        }
+      });
+  },
+
+  postFavoriteActiveOffer: (id, isFavorite) => (dispatch, getState, api) => {
+    const status = isFavorite ? 0 : 1;
+    return api.post(`/favorite/${id}/${status}`)
+      .then((response) => {
+        const adaptedOffer = getAdaptedOffer(response.data);
+        dispatch(ActionCreator.postFavorite(adaptedOffer));
+        dispatch(Operation.loadFavorites());
+        dispatch(OffersActionCreator.changeActiveOffer(adaptedOffer));
+      });
+  }
 };
 
 export default (state = initialState, action) => {
@@ -67,6 +105,18 @@ export default (state = initialState, action) => {
       return extend(state, {
         reviews: action.payload,
       });
+    case ActionType.LOAD_FAVORITES:
+      return extend(state, {
+        favorites: action.payload
+      });
+
+    case ActionType.POST_FAVORITE:
+      return produce(state, (draft) => {
+        const newOffer = action.payload;
+        const index = state.offers.findIndex((_offer) => _offer.id === newOffer.id);
+        draft.offers[index].isFavorite = newOffer.isFavorite;
+      });
+
     case ActionType.POST_REVIEW:
       return extend(state, {
         reviews: action.payload,
