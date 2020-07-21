5c747ee92f5b7146c4cede8a8569f1ebb3f7b017
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-21 01:25:07 -0400

    add private route for favorites page

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Route, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
+
+import {AuthorizationStatus} from "../../reducers/user/user-reducer";
+import {getUserStatus} from "../../reducers/user/selectors";
+import {AppRoute} from "../../const.js";
+
+
+const PrivateRoute = (props) => {
+  const {render, path, exact, userStatus} = props;
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={() => {
+        return (
+          userStatus === AuthorizationStatus.AUTH
+            ? render()
+            : <Redirect to={AppRoute.LOGIN} />
+        );
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  userStatus: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  userStatus: getUserStatus(state)
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
