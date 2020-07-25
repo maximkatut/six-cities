1b36ded4f23e10503b9d7c0012fd7a3512b2fd44
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-07-25 02:19:50 -0400

    add new tests

---
+++ src/components/map/map.e2e.test.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import {Map} from './map.jsx';
+
+import {offers} from '../../test-data/index';
+
+configure({
+  adapter: new Adapter()
+});
+
+describe(`Map e2e`, () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+        <Map
+          activeOffer={offers[0]}
+          activeCityName={`Gomel`}
+          offers={offers}
+          cardIdOnHover={1}
+          offersClosest={[offers[1]]}
+        />
+    );
+  });
+
+  afterAll(() => {
+    wrapper.unmount();
+  });
+  it(`Map should update if card id has been changed`, () => {
+
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      cardIdOnHover: 2
+    });
+
+    wrapper.setProps({
+      cardIdOnHover: -1
+    });
+
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+  it(`Map should update if offersClosest has been changed`, () => {
+
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      offersClosest: [offers[0]]
+    });
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it(`Map should update if activeCityName has been changed`, () => {
+
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      activeCityName: `Paris`
+    });
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it(`Map should update if activeOffer has been changed`, () => {
+
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      activeOffer: offers[1]
+    });
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/offer/offer.e2e.test.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import {Offer} from './offer.jsx';
+import Spinner from '../spinner/spinner.jsx';
+
+import {offers, reviews} from '../../test-data/index';
+import {AuthorizationStatus} from '../../reducers/user/user-reducer.js';
+
+configure({
+  adapter: new Adapter()
+});
+
+describe(`Offer e2e`, () => {
+  let wrapper;
+  const match = {
+    params: {
+      id: `1`
+    }
+  };
+
+  const onFavotireButtonClick = jest.fn();
+  const getOfferData = jest.fn();
+  window.scrollTo = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(
+        <Offer
+          offers={offers}
+          reviews={reviews}
+          offersNearby={[offers[0]]}
+          userStatus={AuthorizationStatus.NO_AUTH}
+          onFavotireButtonClick={onFavotireButtonClick}
+          match={match}
+          location={{loc: `location`}}
+          getOfferData={getOfferData}
+        />
+    );
+  });
+
+  afterAll(() => {
+    wrapper.unmount();
+  });
+
+  it(`Offer should update if id city has been changed`, () => {
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      offers: [offers[1]]
+    });
+
+    expect(spy).toHaveBeenCalled();
+    expect(getOfferData).toHaveBeenCalled();
+  });
+
+  it(`Offer should update if location has been changed and go to top`, () => {
+    const spy = jest.spyOn(wrapper.instance(), `componentDidUpdate`);
+
+    wrapper.setProps({
+      location: {loc: `location2`}
+    });
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(window.scrollTo).toHaveBeenCalledTimes(1);
+  });
+
+  it(`Offer should render Spinner`, () => {
+    expect(wrapper.find(Spinner)).toHaveLength(1);
+  });
+
+  it(`Offer bookmark button should go to login if user no authorized`, () => {
+    wrapper.setProps({
+      location: {loc: `location2`}
+    });
+
+    const button = wrapper.find(`button`);
+    expect(button.hasClass(`property__bookmark-button`)).toBeTruthy();
+    button.simulate(`click`);
+    expect(location.pathname).toBe(`/login`);
+  });
+
+  it(`Offer bookmark button should called cb if user authorized`, () => {
+    wrapper.setProps({
+      userStatus: AuthorizationStatus.AUTH
+    });
+
+    const button = wrapper.find(`button`);
+    expect(button.hasClass(`property__bookmark-button`)).toBeTruthy();
+    button.simulate(`click`);
+    expect(onFavotireButtonClick).toHaveBeenCalledTimes(1);
+    expect(onFavotireButtonClick).toHaveBeenCalledWith(1, true);
+  });
+
+});
